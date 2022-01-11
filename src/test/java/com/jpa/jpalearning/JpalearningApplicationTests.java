aa4a88b2e3974c00e4f83f1c0d2040c37695918f
Gaurav Verma <mr.gaurav245@gmail.com>
2022-01-11 11:03:53 +0530

    Unit testing practice

--- src/test/java/com/jpa/jpalearning/JpalearningApplicationTests.java
+++ src/test/java/com/jpa/jpalearning/JpalearningApplicationTests.java
@@ -3,11 +3,33 @@ package com.jpa.jpalearning;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class JpalearningApplicationTests {
 
+	private Calculator c = new Calculator();
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void testSum(){
+		// Expected
+		int expectedResult = 17;
+		//Actual
+		int actualResult = c.doSum(12, 4, 1);
+		assertThat(actualResult).isEqualTo(expectedResult);
+	}
+
+	@Test
+	void testProduct(){
+		int expectedResult = 6;
+
+		int actualResult = c.doProduct(2, 3);
+		assertThat(actualResult).isEqualTo(expectedResult);
+	}
+
+
 
 }
